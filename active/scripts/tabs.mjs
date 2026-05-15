61b89abc08bb1eea1f158c013f8e39ea2e7d5982
octotoxin <dhruva.neurotoxin@gmail.com>
2026-05-14 21:45:39 -0400

    feat: change default search engine/tab to duckduckgo.com

--- active/scripts/tabs.mjs
+++ active/scripts/tabs.mjs
@@ -51,7 +51,7 @@ pageRefresh.onclick = () => {
 };
 
 newTabButton.onclick = () => {
-  addTab("google.com");
+  addTab("duckduckgo.com");
 };
 
 // Options (opt menu)
@@ -133,7 +133,7 @@ const tabItem = (tab) => {
             if (tabs.length) focusTab(tabs[tabs.length - 1]);
             else
               setTimeout(() => {
-                addTab("google.com");
+                addTab("duckduckgo.com");
               }, 100);
           }
 
@@ -248,7 +248,7 @@ async function addTab(link) {
   focusTab(tab);
 }
 
-addTab("google.com");
+addTab("duckduckgo.com");
 
 const urlParams = new URLSearchParams(window.location.search);
 
